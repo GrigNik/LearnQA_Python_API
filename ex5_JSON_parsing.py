d50d81a6565fb8470038ad277d37f83b380cfa96
GrigNik <142420555+GrigNik@users.noreply.github.com>
2023-08-20 18:45:49 +0300

    Add files via upload

---
+++ ex5_JSON_parsing.py
@@ -0,0 +1,9 @@
+import json
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(json_text)
+import json
+json_text2 = obj['messages'][1]
+print(json_text2['message'])
+
+
+
